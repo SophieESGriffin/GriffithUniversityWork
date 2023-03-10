d9fa88d88e64f0f0d05e2de9a33292e341d2026b
SophieESGriffin <126839833+SophieESGriffin@users.noreply.github.com>
2023-03-10 14:56:45 +1000

    Initial Commit - Sketches

---
+++ Firstyear/1701ICT/Week1/MickeyMouse/sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(500, 500);
+}
+
+function draw() {
+  background(170,10,200);
+  fill(100,0,50)
+  circle(120,170,100)
+  circle(350,170,100)
+  fill(100,10,100)
+  circle(240,270,250)
+  fill(10,10,10)
+  circle(190,250,50)
+  circle(290,250,50)
+  circle(240,320,70)
+ 
+}
---
+++ Firstyear/1701ICT/Week1/Robot/sketch.js
@@ -0,0 +1,21 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(150);
+  rect(100,100,150,100,20)
+  fill(100,0,0)
+  circle(140,130,30)
+  fill(0,0,100)
+  circle(210,130,30)
+  fill(150)
+  rect(140,160,70,30)
+  fill(100)
+  rect(130,200,90,150)
+  rect(130,350,30,30)
+  rect(190,350,30,30)
+  rect(100,220,30,80)
+  rect(220,220,30,80)
+  
+}
