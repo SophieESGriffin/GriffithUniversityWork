35d25ffd5b9c0c2e346b6d5515cc385c38102f63
SophieESGriffin <126839833+SophieESGriffin@users.noreply.github.com>
2023-03-16 16:26:53 +1000

    Drawing lines then multiplying them using variables

---
+++ Firstyear/1701ICT/Week1/Lines using variables/sketch.js
@@ -0,0 +1,44 @@
+var x1=0;
+var y1=0;
+var x2=0;
+var y2=0;
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  
+line(x1,y1,x2,y2)
+  
+  var x1 = 50
+  var y1 = 50
+  var x2 = 100
+  var y2 = 100
+  
+  line(x1,y1,x2,y2)
+  
+
+  y1 = y1 + 5
+  y2 = y2 + 5
+  line(x1,y1,x2,y2)
+    
+  y1 = y1 + 5
+  y2 = y2 + 5
+  line(x1,y1,x2,y2)
+  
+  y1 = y1 + 5
+  y2 = y2 + 5
+  line(x1,y1,x2,y2)
+  
+  y1 = y1 + 5
+  y2 = y2 + 5
+  line(x1,y1,x2,y2)
+  
+  y1 = y1 + 5
+  y2 = y2 + 5
+  line(x1,y1,x2,y2)
+  
+
+}
