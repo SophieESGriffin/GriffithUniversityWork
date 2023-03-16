ee8d15c454e317e58c5a6fa0d4e3d6acb50cc9dc
SophieESGriffin <126839833+SophieESGriffin@users.noreply.github.com>
2023-03-16 16:30:14 +1000

    Edit

---
+++ Firstyear/1701ICT/Week2/Overlappingcircles/sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  fill(255,255,255);
+  noStroke();
+  ellipse(56,46,55,55);
+  fill(255,100,100,128);
+  stroke(255,0,0);
+  ellipse(80,80,55,55);
+  ellipse(110,110,55,55)
+  
+}
